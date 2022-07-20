415ea3d4d197dea29cf9f70ca72fe893a2c3df7b
André Ferreira da Cruz <andre@pop-os.localdomain>
2022-07-19 21:02:46 -0300

    inicio

---
+++ frontend/src/user/pages/Users.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import UsersList from '../components/UsersList';
+
+const Users = () => {
+    const USERS = [{
+        id: 'u1',
+        name: 'Max Verstappen',
+        image: 'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        places: 3
+    }];
+
+    return <UsersList items={USERS} />;
+};
+
+export default Users;
